0ff222917194aa60d065b0c633fbf6a7c7ca0063
ViniScooper <vviniciuslourenco@gmail.com>
2026-04-07 16:54:53 -0300

    plataforma

--- template_react_login/api_users/src/index.js
+++ template_react_login/api_users/src/index.js
@@ -3,9 +3,8 @@ import { app } from './server.js'
 
 
 configDotenv()
-const { PORT_API } = process.env
 
-const port = process.env.PORT_API || 3000
+const port = process.env.PORT || process.env.PORT_API || 3000
 
 app.listen(port, '0.0.0.0', () => {
   console.log('Servidor rodando na porta:', port, 'no IP 0.0.0.0')
